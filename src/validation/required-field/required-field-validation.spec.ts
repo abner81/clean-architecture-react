4ec93784ab507994af6029b317527f47a78f4c0d
Abner Machado <54149914+abner81@users.noreply.github.com>
2021-01-21 14:27:07 -0300

    refactor: move SUT creaton to a facotory method

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -2,15 +2,20 @@ import { RequiredFieldError } from "../errors";
 import { RequiredFieldValidaton } from "./required-field-validation";
 import faker from "faker";
 
+const makeSut = (): RequiredFieldValidaton => {
+  const sut = new RequiredFieldValidaton("email");
+  return sut;
+};
+
 describe("RequiredFieldValidation", () => {
   test("should return error if field is empty ", () => {
-    const sut = new RequiredFieldValidaton("email");
+    const sut = makeSut();
     const error = sut.validate("");
     expect(error).toEqual(new RequiredFieldError());
   });
 
   test("should return falsy if field is not empty ", () => {
-    const sut = new RequiredFieldValidaton("email");
+    const sut = makeSut();
     const error = sut.validate(faker.random.word());
     expect(error).toBeFalsy();
   });
