e2e1ee183b2c2303086ebc46d696b4e462a9147e
Abner Machado <54149914+abner81@users.noreply.github.com>
2021-01-24 15:42:22 -0300

    refactor: change api url factory to receive path by param

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     '<rootDir>/src/**/*.{ts, tsx}',
     '!<rootDir>/src/main/**/*',
     '!<rootDir>/src/presentation/components/router/**/*',
+    '!<rootDir>/src/domain/**/*',
     '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
