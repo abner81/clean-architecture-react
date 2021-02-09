2025d49aa668ce306ed98046d823def2722e9e2f
Abner Machado <54149914+abner81@users.noreply.github.com>
2021-02-09 13:44:18 -0300

    test: ensure RemoteAddAccount returns UnexpectedError if HttpStatusCode returns 500

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -64,4 +64,13 @@ describe("RemoteAddAccount", () => {
     const promise = sut.add(mockAddAccountParams());
     await expect(promise).rejects.toThrow(new UnexpectedError());
   });
+
+  test("Shoud throw UnexpectedError if HttpPostClient returns 500", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError,
+    };
+    const promise = sut.add(mockAddAccountParams());
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
 });
