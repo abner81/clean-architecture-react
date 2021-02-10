4b8efe42a0a1df7ee5ceb8a0a5b55394931a57e8
Abner Machado <54149914+abner81@users.noreply.github.com>
2021-02-10 12:41:58 -0300

    feat: ensure SignUp starts with correct initial state

---
+++ src/presentation/pages/signup/signup.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { Header, Footer, Input, FormStatus } from "@/presentation/components";
+import Context from "@/presentation/contexts/form/form-context";
+import Styles from "./signup-styles.scss";
+
+const SignUp: React.FC = () => {
+  const [state, setState] = useState({
+    isLoading: false,
+    nameError: "",
+    emailError: "Campo Obrighatório",
+    passwordError: "Campo Obrighatório",
+    passwordConfirmationError: "Campo Obrighatório",
+    mainError: "",
+  });
+
+  return (
+    <div className={Styles.signup}>
+      <Header />
+      <Context.Provider value={{ state }}>
+        <form className={Styles.form}>
+          <h2>Criar Conta</h2>
+          <Input type="text" name="name" placeholder="Digite seu nome" />
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
+          <Input
+            type="password"
+            name="passwordConfirmation"
+            placeholder="Repita sua senha"
+          />
+          <button
+            disabled
+            className={Styles.submit}
+            data-testid="submit"
+            type="submit"
+          >
+            Entrar
+          </button>
+          <span className={Styles.link}>Voltar para Login</span>
+          <FormStatus />
+        </form>
+      </Context.Provider>
+      <Footer />
+    </div>
+  );
+};
+
+export default SignUp;
