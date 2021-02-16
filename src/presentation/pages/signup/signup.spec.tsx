824c4912e5a98b52a96f5f794905d938237cc2d9
Abner Machado <54149914+abner81@users.noreply.github.com>
2021-02-16 14:11:38 -0300

    feat: ensure SignUp prevent AddAccount to be called multiple times

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -131,4 +131,11 @@ describe("SignUp Component", () => {
       name,
     });
   });
+
+  test("Should call AddAccount only once", async () => {
+    const { sut, addAccountSpy } = makeSut();
+    await simulateValidSubmit(sut);
+    await simulateValidSubmit(sut);
+    expect(addAccountSpy.callsCount).toBe(1);
+  });
 });
