ac5eefb6a47630a1ec274fb990c901675f51ce08
Abner Machado <54149914+abner81@users.noreply.github.com>
2021-03-06 12:32:27 -0300

    feat: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.tsx
@@ -0,0 +1,17 @@
+import {
+  ValidationComposite,
+  ValidationBuilder,
+} from "@/validation/validators";
+import { Validation } from "@/presentation/protocols/validation";
+
+export const makeSignUpValidation = (): Validation => {
+  return ValidationComposite.build([
+    ...ValidationBuilder.field("name").required().min(5).build(),
+    ...ValidationBuilder.field("email").required().email().build(),
+    ...ValidationBuilder.field("password").required().min(5).build(),
+    ...ValidationBuilder.field("passwordConfirmation")
+      .required()
+      .min(5)
+      .build(),
+  ]);
+};
