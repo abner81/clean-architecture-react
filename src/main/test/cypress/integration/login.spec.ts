a93471ec061e0152457eea8eeec560f41416ccee
Abner Machado <54149914+abner81@users.noreply.github.com>
2021-03-11 19:51:47 -0300

    test: ensure Login save accessToken if valid credentials are provided

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,6 @@
 import faker from "faker";
 
+const baseUrl: string = Cypress.config().baseUrl;
 describe("Login", () => {
   beforeEach(() => cy.visit("login"));
   it("should load with correct inital state", () => {
@@ -29,5 +30,36 @@ describe("Login", () => {
 
     cy.getByTestId("submit").should("have.attr", "disabled");
     cy.getByTestId("error-wrap").should("not.have.descendants");
+  });
+  it("should present error if invalid credentials are provided", () => {
+    cy.getByTestId("email").focus().type(faker.internet.email());
+    cy.getByTestId("password").focus().type(faker.random.alphaNumeric(5));
+    cy.getByTestId("submit").click();
+    cy.getByTestId("error-wrap")
+      .getByTestId("spinner")
+      .should("exist")
+      .getByTestId("main-error")
+      .should("not.exist")
+      .getByTestId("spinner")
+      .should("not.exist")
+      .getByTestId("main-error")
+      .should("contain.text", "Credenciais inválidas");
+    cy.url().should("eq", `${baseUrl}/login`);
+  });
+  it("should save accessToken if valid credentials are provided", () => {
+    cy.getByTestId("email").focus().type("mango@gmail.com");
+    cy.getByTestId("password").focus().type("12345");
+    cy.getByTestId("submit").click();
+    cy.getByTestId("error-wrap")
+      .getByTestId("spinner")
+      .should("exist")
+      .getByTestId("main-error")
+      .should("not.exist")
+      .getByTestId("spinner")
+      .should("not.exist");
+    cy.url().should("eq", `${baseUrl}/`);
+    cy.window().then((window) =>
+      assert.isOk(window.localStorage.getItem("accessToken"))
+    );
   });
 });
