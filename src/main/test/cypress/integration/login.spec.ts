511ff7697579cdc586265c4dadd8414a6d137653
Abner Machado <54149914+abner81@users.noreply.github.com>
2021-03-12 13:45:16 -0300

    test: ensure present unexpected error  if invalid data is returned

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -34,7 +34,7 @@ describe("Login", () => {
     cy.getByTestId("submit").should("have.attr", "disabled");
     cy.getByTestId("error-wrap").should("not.have.descendants");
   });
-  it("should present error if invalid credentials are provided", () => {
+  it("should present InvalidCredentialsError on 400", () => {
     cy.route({
       method: "POST",
       url: /login/,
@@ -53,13 +53,51 @@ describe("Login", () => {
     );
     cy.url().should("eq", `${baseUrl}/login`);
   });
+  it("should present UnexpectedError on 401", () => {
+    cy.route({
+      method: "POST",
+      url: /login/,
+      status: 400,
+      response: {
+        error: faker.random.words(),
+      },
+    });
+    cy.getByTestId("email").focus().type(faker.internet.email());
+    cy.getByTestId("password").focus().type(faker.random.alphaNumeric(5));
+    cy.getByTestId("submit").click();
+    cy.getByTestId("spinner").should("not.exist");
+    cy.getByTestId("main-error").should(
+      "contain.text",
+      "Algo de errado aconteceu. Tente novamente em breve."
+    );
+    cy.url().should("eq", `${baseUrl}/login`);
+  });
+  it("should present unexpected error if invalid data is returned", () => {
+    cy.route({
+      method: "POST",
+      url: /login/,
+      status: 200,
+      response: {
+        invalidProperty: faker.random.uuid(),
+      },
+    });
+    cy.getByTestId("email").focus().type("mango@gmail.com");
+    cy.getByTestId("password").focus().type("12345");
+    cy.getByTestId("submit").click();
+    cy.getByTestId("main-error").should("not.exist");
+    cy.getByTestId("spinner").should("not.exist");
+    cy.url().should("eq", `${baseUrl}/`);
+    cy.window().then((window) =>
+      assert.isOk(window.localStorage.getItem("accessToken"))
+    );
+  });
   it("should save accessToken if valid credentials are provided", () => {
     cy.route({
       method: "POST",
       url: /login/,
       status: 200,
       response: {
-        ssx: faker.random.uuid(),
+        accessToken: faker.random.uuid(),
       },
     });
     cy.getByTestId("email").focus().type("mango@gmail.com");
