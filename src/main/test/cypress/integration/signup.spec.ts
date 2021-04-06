55a31a558914cb4ccf816fd32bf0f3d225f44844
Abner Machado <54149914+abner81@users.noreply.github.com>
2021-04-06 20:56:31 -0300

    test: ensure SignUp present error if form is invalid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -7,17 +7,35 @@ describe("Login", () => {
     cy.visit("signup");
   });
   it("should load with correct inital state", () => {
-    FormHelper.testInputStatus("email", "Campo Obrigatório");
-    cy.getByTestId("email").should("have.attr", "readOnly");
-
     FormHelper.testInputStatus("name", "Campo Obrigatório");
     cy.getByTestId("name").should("have.attr", "readOnly");
 
+    FormHelper.testInputStatus("email", "Campo Obrigatório");
+    cy.getByTestId("email").should("have.attr", "readOnly");
+
     FormHelper.testInputStatus("password", "Campo Obrigatório");
     cy.getByTestId("password").should("have.attr", "readOnly");
-
     FormHelper.testInputStatus("passwordConfirmation", "Campo Obrigatório");
     cy.getByTestId("passwordConfirmation").should("have.attr", "readOnly");
+
+    cy.getByTestId("submit").should("have.attr", "disabled");
+    cy.getByTestId("error-wrap").should("not.have.descendants");
+  });
+
+  it("should present error state if form is invalid", () => {
+    cy.getByTestId("name").focus().type(faker.random.alphaNumeric(3));
+    FormHelper.testInputStatus("name", "Valor inválido");
+
+    cy.getByTestId("email").focus().type(faker.random.word());
+    FormHelper.testInputStatus("email", "Valor inválido");
+
+    cy.getByTestId("password").focus().type(faker.random.alphaNumeric(3));
+    FormHelper.testInputStatus("password", "Valor inválido");
+
+    cy.getByTestId("passwordConfirmation")
+      .focus()
+      .type(faker.random.alphaNumeric(4));
+    FormHelper.testInputStatus("passwordConfirmation", "Valor inválido");
 
     cy.getByTestId("submit").should("have.attr", "disabled");
     cy.getByTestId("error-wrap").should("not.have.descendants");
